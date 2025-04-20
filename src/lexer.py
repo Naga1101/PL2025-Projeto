81a259ac74ba7e51e1947ecb17e001135e2f5a09
Nuno Aguiar <116183745+Naga1101@users.noreply.github.com>
2025-04-20 22:33:02 +0100

    Start of lexer

---
+++ src/lexer.py
@@ -0,0 +1,25 @@
+import ply.lex as lex
+import json
+import sys
+
+tokens = (
+    'BEGIN',
+    'ID' # o parser é que decide se é uma var ou o nome de uma func 
+)
+
+keywords = {
+    'begin': 'BEGIN',
+}
+
+def t_ID(t):
+    r'\w{1}[a-zA-Z0-9]*'
+    t.type = keywords.get(t.value.lower(), 'ID')
+    return t
+
+t_ignore = ' \t\n'
+
+def t_error(t):
+    print(f"Illegal character '{t.value[0]}'")
+    t.lexer.skip(1)
+
+lexer = lex.lex()
