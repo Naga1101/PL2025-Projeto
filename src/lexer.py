7a20de378e1763eef05908556f6ab4c948263b8d
TomasValente <116099924+TomasValente@users.noreply.github.com>
2025-04-24 11:28:27 +0100

    Addition of tokens

--- src/lexer.py
+++ src/lexer.py
@@ -4,12 +4,45 @@ import sys
 
 tokens = (
     'BEGIN',
-    'ID' # o parser é que decide se é uma var ou o nome de uma func 
+    'LPAREN',     # (
+    'RPAREN',     # )
+    'COMMA',      # ,
+    'COLON',      # :
+    'SEMICOLON',  # ;
+    'DOT',        # .
+    'ASSIGN',     # :=
+    'STRING',     # 'string'
+    'NUMBER',     # 123
+    'ID' # o parser é que decide se é uma var ou o nome de uma func
 )
 
 keywords = {
     'begin': 'BEGIN',
 }
+
+#Regular expressions
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_COMMA = r','
+t_COLON = r':'
+t_SEMICOLON = r';'
+t_DOT = r'\.'
+t_ASSIGN = r':='
+
+# Literais numéricos
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# Literais de strings/char
+def t_STRING(t):
+    r'\'([^\'\n]|(\\\'))*\''
+    value = t.value[1:-1]
+    if len(value) == 1:
+        t.type = 'CHAR'
+    t.value = value
+    return t
 
 def t_ID(t):
     r'^[a-zA-Z_]{1}[a-zA-Z0-9_]*'
